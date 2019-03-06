1d4ccea8899e0bbd2856a08691d1802562cd66b0
Ian-Cornelius <ianomondi27@gmail.com>
2019-03-06 12:27:03 +0300

    Wired UI, missing charts

--- app/src/main/java/jkuatcs/com/qccharts/models/QData.java
+++ app/src/main/java/jkuatcs/com/qccharts/models/QData.java
@@ -16,6 +16,8 @@ public class QData {
         this.r = r;
     }
 
+    //Doing x_bar and r analysis on data entry (sample sizes)
+    //Formulate a function to check size of entered data, to ensure as big as sample size, before storing
     public QData(String coma_values){
         this.size_values = coma_values;
         double total = 0;
@@ -28,14 +30,22 @@ public class QData {
             total+=d;
         }
 
+        //Finally extracted x_bar and r values
         this.x_bar = total/getSizesAsDoubles().size();
         this.r = largest-smallest;
     }
 
+    //Function to change string to double values
+    //Ensure split has no letter in it, in validation function
+
     public ArrayList<Double> getSizesAsDoubles(){
         ArrayList<Double> all = new ArrayList<>();
         for(String s: size_values.split(",")){
-            all.add(Double.valueOf(s));
+
+            if (!s.isEmpty()){
+
+                all.add(Double.valueOf(s));
+            }
         }
         return all;
     }
